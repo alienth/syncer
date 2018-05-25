7a5e1d69af0ebeb862077194b1c6735c1b7e9fc9
Jason Harvey <alienth@gmail.com>
2018-05-24 22:39:25 -0800

    progress

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
+	// "path/filepath"
+	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/fsnotify/fsnotify"
@@ -11,6 +15,7 @@ import (
 
 // Continuous sync in either direction
 // Build manifest and store that remotely.
+// option - build manifest every X minutes, or every X events
 // Optionally delete things remotely that don't exist locally.
 // Exclude/include filters.
 // List manifests.
@@ -18,25 +23,56 @@ import (
 // Sign contents.
 
 func main() {
+	source := "/tmp/boo"
+	recurse := true
+
 	watcher, _ := fsnotify.NewWatcher()
-	watcher.Add("/tmp")
+	watcher.Add(source)
+	if recurse {
+		files, err := ioutil.ReadDir(source)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			if f.IsDir() {
+				watcher.Add(source + "/" + f.Name())
+			}
+		}
+	}
 
 	sess, err := session.NewSession()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// /tmp/bar/        -> s3://alienthtest/
+	// /tmp/bar/foo     -> s3://alienthtest/foo
+	// /tmp/bar/foo/bar -> s3://alienthtest/foo/bar
+
 	svc := s3.New(sess)
 	for {
 		select {
 		case event := <-watcher.Events:
-			bucket := "s3://alienth"
-			f, _ := os.Open(event.Name)
-			foo := s3.PutObjectInput{
-				Bucket: &bucket,
-				Body:   f,
+			bucket := "alienthtest"
+			fi, _ := os.Lstat(event.Name)
+			if fi.IsDir() {
+				if recurse {
+					watcher.Add(event.Name)
+				}
+				continue
 			}
-			log.Println(foo, svc)
+			f, _ := os.Open(event.Name)
+			log.Println(event)
+			foo := s3.PutObjectInput{
+				Bucket: aws.String(bucket),
+				Body:   f,
+				Key:    aws.String(strings.TrimPrefix(event.Name, source)),
+			}
+			_, err := svc.PutObject(&foo)
+			if err != nil {
+				log.Fatal(err)
+			}
 
 		}
 	}
